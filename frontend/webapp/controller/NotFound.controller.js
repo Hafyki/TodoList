c47223885c65121c0a2e7cb0d68e4f7e60315597
Hafy Kouzak <hafymjck@gmail.com>
2025-10-27 23:10:22 -0300

    feature: Adicionando view de 404 NotFound

---
+++ frontend/webapp/controller/NotFound.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["com/todolist/controller/BaseController"],
+    function(BaseController){
+        "use strict";
+        return BaseController.extend(
+            "com.todolist.controller.NotFound",
+            {
+                onInit: function(){}
+            }
+        );
+    }
+);
